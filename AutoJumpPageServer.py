4843d3035169dbc31efe665d7d60f4a6c1b9878d
hesiyuan <hesiyuan117@qq.com>
2017-12-06 10:36:52 +0800

    add project

---
+++ AutoJumpPageServer.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask
+from flask import redirect
+
+
+app = Flask(__name__)
+
+@app.route('/',methods=['GET','POST'])
+def Index():
+    lUrl = "http://dumpupload.fanxing.com/fxlist.php?date=%s&format=link"%GetLastday()
+    return  redirect(lUrl)
+
+
+def GetLastday(tDays= 1):
+    import datetime
+    today = datetime.date.today()
+    oneday = datetime.timedelta(days=tDays)
+    iLastday = today - oneday
+    return  iLastday.strftime("%Y-%m-%d")
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",debug=True,port=8081)
